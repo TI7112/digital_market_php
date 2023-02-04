e4d234e142c2c873f398895cd0058fa39ae85e25
ti7112 <ti7112@outlook.com>
2023-02-04 10:52:00 +0530

    first commit

---
+++ components/header.php
@@ -0,0 +1,13 @@
+<div class="flex justify-between items-center px-16 h-44">
+    <img class="" src="assets/img-primary/logo.png" alt="">
+    <div class="flex font-semibold items-center text-white gap-16">
+        <a class="text-xl hover:text-teal-500 duration-300" href="index.php">Home</a>
+        <a class="text-xl hover:text-teal-500 duration-300" href="about.php">About</a>
+        <a class="text-xl hover:text-teal-500 duration-300" href="workshop.php">Workshop</a>
+        <a class="text-xl hover:text-teal-500 duration-300" href="contact.php">Contact</a>
+        <form class="flex items-center px-2 border-2 rounded-full border-teal-500 py-1" method="get">
+            <input size="45" class="bg-transparent outline-none pl-4" type="search" name="search" id="">
+            <input class="bg-transparent px-2 text-xl" type="submit" value="Search">
+        </form>
+    </div>
+</div>
